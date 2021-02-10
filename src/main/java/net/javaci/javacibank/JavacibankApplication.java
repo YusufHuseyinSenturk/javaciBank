5a90e5288910b5e4589496245844bee335267f8a
YusufHuseyinSenturk <yusufhuseyinsenturk@gmail.com>
2021-02-10 21:58:39 +0300

    Baslangıc

---
+++ src/main/java/net/javaci/javacibank/JavacibankApplication.java
@@ -0,0 +1,13 @@
+package net.javaci.javacibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacibankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacibankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaci/javacibank/JavacibankApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaci.javacibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
